5792f5f370cc1ac11812ef3d2ee56a897b8d56f4
Bhanavi2007 <bhanavipandey2020@gmail.com>
2025-09-11 11:54:57 +0530

    Add files via upload

---
+++ number between 1-7.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int dayNumber;
+
+    printf("Enter a number between 1 and 7 to represent a day of the week: ");
+    scanf("%d", &dayNumber);
+
+
+    switch (dayNumber) {
+        case 1:
+            printf("Day %d is Sunday.\n", dayNumber);
+            break;
+        case 2:
+            printf("Day %d is Monday.\n", dayNumber);
+            break;
+        case 3:
+            printf("Day %d is Tuesday.\n", dayNumber);
+            break;
+        case 4:
+            printf("Day %d is Wednesday.\n", dayNumber);
+            break;
+        case 5:
+            printf("Day %d is Thursday.\n", dayNumber);
+            break;
+        case 6:
+            printf("Day %d is Friday.\n", dayNumber);
+            break;
+        case 7:
+            printf("Day %d is Saturday.\n", dayNumber);
+            break;
+        default:
+            printf("Invalid input. Please enter a number between 1 and 7.\n");
+            break;
+    }
+
+    return 0;
+}
