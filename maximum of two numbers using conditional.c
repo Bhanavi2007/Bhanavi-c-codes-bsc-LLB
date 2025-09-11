23bdb3d8f445502ab7956c1f8302adf76ea72d44
Bhanavi2007 <bhanavipandey2020@gmail.com>
2025-09-11 11:49:21 +0530

    Add files via upload

---
+++ maximum of two numbers using conditional.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, maximum;
+
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    maximum = (num1 > num2) ? num1 : num2;
+
+    printf("The maximum of %d and %d is %d\n", num1, num2, maximum);
+
+    return 0;
+}
