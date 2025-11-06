a721972acddd0ed5b66c4f6e33d442dc29002a23
Bhanavi2007 <bhanavipandey2020@gmail.com>
2025-11-07 01:32:44 +0530

    Update sum of n natural numbers.c

--- sum of n natural numbers.c
+++ sum of n natural numbers.c
@@ -3,19 +3,20 @@
 int main() {
     int n, sum = 0;
 
-    // Prompt the user to enter a positive integer
+    
     printf("Enter a positive integer: ");
 
-    // Read the integer from the user
+    
     scanf("%d", &n);
 
-    // Calculate the sum using a for loop
+
     for (int i = 1; i <= n; i++) {
-        sum += i; // Add each number from 1 to n to the sum
+        sum += i; 
     }
 
-    // Display the calculated sum
+    
     printf("Sum of the first %d natural numbers is: %d\n", n, sum);
 
     return 0;
 }
+
