7859469f7f0194ded052b56952dd6e4afd1b6237
Bhanavi2007 <bhanavipandey2020@gmail.com>
2025-11-06 12:52:57 +0530

    Add files via upload

---
+++ key in an array of 10 integers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    int key, i, found = 0;
+
+    printf("Enter 10 integers:\n");
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the key to search: ");
+    scanf("%d", &key);
+
+    for (i = 0; i < 10; i++) {
+        if (arr[i] == key) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (found) {
+        printf("Key found at index %d\n", i);
+    } else {
+        printf("Key not found in the array\n");
+    }
+
+    return 0;
+}
