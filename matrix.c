9af6804518ff59d7d4f17ce44fe61fb766160bb0
Bhanavi2007 <bhanavipandey2020@gmail.com>
2025-11-06 13:10:43 +0530

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int matrix1[3][3], matrix2[3][3], sumMatrix[3][3];
+    int i, j;
+
+    printf("Enter elements of first 3x3 matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter elements of second 3x3 matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    printf("Sum of the two matrices:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", sumMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
